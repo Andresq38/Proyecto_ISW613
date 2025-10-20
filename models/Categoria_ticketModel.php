c3206ea1b4844597f354b70d6096a69091db9578
402520286 <andresscastilloo81@gmail.com>
2025-10-19 22:17:16 -0600

    Class categoria_ticketController added method to list the specialties  based on the ID from category

--- models/Categoria_ticketModel.php
+++ models/Categoria_ticketModel.php
@@ -21,12 +21,45 @@ class Categoria_ticketModel
             handleException($e);
         }
     }
+    
     /*Obtener */
     public function get($id)
     {
         try {
             //Consulta sql
 			$vSql = "SELECT * FROM categoria_ticket where id_categoria=$id";
+			
+            //Ejecutar la consulta
+			$vResultado = $this->enlace->ExecuteSQL ( $vSql);
+			// Retornar el objeto
+			return $vResultado[0];
+		} catch (Exception $e) {
+            handleException($e);
+        }
+    }
+
+        /*Obtener detalle de categoría según selección del usuario*/
+    public function getCategoria($id)
+    {
+        try {
+            //Consulta sql
+			$vSql = "
+            SELECT 
+                c.id_categoria,
+                c.nombre AS categoria,
+                s.tiempo_respuesta_max,
+                s.tiempo_resolucion_max,
+                GROUP_CONCAT(DISTINCT e.nombre ORDER BY e.nombre SEPARATOR ', ') AS etiquetas,
+                GROUP_CONCAT(DISTINCT esp.nombre ORDER BY esp.nombre SEPARATOR ', ') AS especialidades
+            FROM categoria_ticket c
+            INNER JOIN sla s ON c.id_sla = s.id_sla
+            LEFT JOIN categoria_etiqueta ce ON ce.id_categoria_ticket = c.id_categoria
+            LEFT JOIN etiqueta e ON e.id_etiqueta = ce.id_etiqueta
+            LEFT JOIN categoria_especialidad cesp ON cesp.id_categoria_ticket = c.id_categoria
+            LEFT JOIN especialidad esp ON esp.id_especialidad = cesp.id_especialidad
+            WHERE c.id_categoria = $id
+            GROUP BY c.id_categoria, c.nombre, s.tiempo_respuesta_max, s.tiempo_resolucion_max
+        ";
 			
             //Ejecutar la consulta
 			$vResultado = $this->enlace->ExecuteSQL ( $vSql);
