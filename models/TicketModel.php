3645093154df16e6c5c04f71cf88d6ed448261b6
Andresq38 <a.quesadam308@gmail.com>
2025-10-21 00:29:32 -0600

    Changes Hora

--- models/TicketModel.php
+++ models/TicketModel.php
@@ -15,11 +15,12 @@ class TicketModel
                         c.nombre AS 'Categoría',
                         e.nombre AS 'Estado actual',
                         CONCAT(
-                        FLOOR((s.tiempo_resolucion_max - TIMESTAMPDIFF(MINUTE, t.fecha_creacion, NOW())) / 60), 
-                         'h ',
-                        MOD((s.tiempo_resolucion_max - TIMESTAMPDIFF(MINUTE, t.fecha_creacion, NOW())), 60),
-                         'm'
-                         ) AS 'Tiempo restante SLA (máx)'
+                            FLOOR((s.tiempo_resolucion_max - TIMESTAMPDIFF(MINUTE, 
+                            CONVERT_TZ(t.fecha_creacion, '+00:00', '-06:00'), 
+                            NOW())) / 60), 'h ',
+                            MOD((s.tiempo_resolucion_max - TIMESTAMPDIFF(MINUTE, 
+                            CONVERT_TZ(t.fecha_creacion, '+00:00', '-06:00'), 
+                            NOW())), 60),'m') AS 'Tiempo restante SLA (máx)'
                         FROM 
                             ticket t
                         JOIN 
@@ -30,7 +31,6 @@ class TicketModel
                             sla s ON c.id_sla = s.id_sla
                         ORDER BY 
                             t.id_ticket;";
-			
             //Ejecutar la consulta
 			$vResultado = $this->enlace->ExecuteSQL ($vSql);
 				
@@ -93,24 +93,7 @@ class TicketModel
             handleException($e);
         }
     }
-     //POR EL MOMENTO PUESTO EN COMENTARIO POR PRUEBAS
-    /*Obtener los actores de una pelicula */
-    /*/public function getActorMovie($idMovie)
-    {
-        try {
-            //Consulta SQL
-            $vSQL = "SELECT g.id, g.fname, g.lname, mg.role".
-            " FROM actor g, movie_cast mg".
-            " where g.id=mg.actor_id and mg.movie_id=$idMovie;";
-            //Establecer conexión
-            
-            //Ejecutar la consulta
-            $vResultado = $this->enlace->executeSQL($vSQL);
-            //Retornar el resultado
-            return $vResultado;
-        } catch (Exception $e) {
-            handleException($e);
-        }
-    }*/
+
+
     
 }
